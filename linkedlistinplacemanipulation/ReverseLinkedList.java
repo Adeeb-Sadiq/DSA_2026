e76fe54acdfc46f0a44d706864fcd25aafb6836f
Adeeb-Sadiq <adeeb5.sadiq7@gmail.com>
2026-04-07 20:48:41 +0530

    reverse linked list - in place manipulation

---
+++ linkedlistinplacemanipulation/ReverseLinkedList.java
@@ -0,0 +1,46 @@
+package linkedlistinplacemanipulation;
+
+public class ReverseLinkedList {
+    static void main() {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        System.out.println("Original list");
+        printList(head);
+        System.out.println("Reversed list");
+        head = reverseList(head);
+        printList(head);
+    }
+
+    static ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while(curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+
+    static void printList(ListNode node) {
+        while(node != null) {
+            System.out.print(node.val + " ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
