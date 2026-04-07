13623f92beca3f3b037eec3b1dd6617151d75b7a
Adeeb-Sadiq <adeeb5.sadiq7@gmail.com>
2026-04-08 01:44:54 +0530

    remove duplicates in sorted linkedlist

---
+++ linkedlistinplacemanipulation/RemoveDuplicates.java
@@ -0,0 +1,28 @@
+package linkedlistinplacemanipulation;
+
+public class RemoveDuplicates {
+    static void main() {
+        ListNode head = new ListNode(1);
+        ListNode second = new ListNode(1); head.next = second;
+        ListNode third = new ListNode(2); second.next = third;
+        ListNode fourth = new ListNode(3); third.next = fourth;
+        ListNode fifth = new ListNode(4); fourth.next = fifth;
+        ListNode sixth = new ListNode(4); fifth.next = sixth;
+        ListNode seventh = new ListNode(5); sixth.next = seventh;
+        System.out.println("original list");
+        ReverseLinkedList.printList(head);
+        System.out.println("no duplicate list");
+        head = removeDuplicates(head);
+        ReverseLinkedList.printList(head);
+
+    }
+
+    public static ListNode removeDuplicates(ListNode head) {
+        ListNode curr = head;
+        while(curr != null && curr.next != null) {
+            if(curr.val == curr.next.val) curr.next = curr.next.next;
+            else curr = curr.next;
+        }
+        return head;
+    }
+}
