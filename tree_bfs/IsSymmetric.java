c28fc4859224191d72fa1bcdcf4937a8a7538ee3
Adeeb-Sadiq <adeeb5.sadiq7@gmail.com>
2026-04-15 00:49:51 +0530

    is symmetric tree using bfs

--- tree_bfs/IsSymmetric.java
+++ tree_bfs/IsSymmetric.java
@@ -1,5 +1,10 @@
 package tree_bfs;
 
+import org.w3c.dom.Node;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class IsSymmetric {
     public static boolean isSymmetric (TreeNode root) {
         if(root == null) return true;
@@ -13,8 +18,36 @@ public class IsSymmetric {
         return isMirror(left.left, right.right) && isMirror(left.right, right.left);
     }
 
+    public static boolean isSymmetric_bfs (TreeNode root) {
+        if(root == null) return true;
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root.left);
+        q.offer(root.right);
+
+        while(!q.isEmpty()) {
+            TreeNode left = q.poll();
+            TreeNode right = q.poll();
+
+            if(left == null && right == null) continue;
+
+            if(left == null || right == null || left.val != right.val) return false;
+
+            q.offer(left.left);
+            q.offer(right.right);
+            q.offer(left.right);
+            q.offer(right.left);
+        }
+
+        return true;
+    }
+
     static void main() {
+        System.out.println("DFS");
         System.out.println(isSymmetric(TreeNode.getTree()));
         System.out.println(isSymmetric(TreeNode.SymmetricTree()));
+        System.out.println("BFS");
+        System.out.println(isSymmetric_bfs(TreeNode.getTree()));
+        System.out.println(isSymmetric_bfs(TreeNode.SymmetricTree()));
     }
 }
