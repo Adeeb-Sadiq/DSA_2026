f9c0331f919f4d82a60cc06e1f1b952c762e1a64
Adeeb-Sadiq <adeeb5.sadiq7@gmail.com>
2026-04-27 02:55:40 +0530

    is anagram

---
+++ hashmaps/IsAnagram.java
@@ -0,0 +1,31 @@
+package hashmaps;
+
+import java.util.HashMap;
+
+public class IsAnagram {
+    public static boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for(char c: s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        for(char c: t.toCharArray()) {
+            if(!map.containsKey(c)) return false;
+            map.put(c, map.get(c) - 1);
+
+            if(map.get(c) == 0) {
+                map.remove(c);
+            }
+        }
+
+        return map.isEmpty();
+    }
+
+    static void main() {
+        String s = "racecar";
+        String t = "carrace";
+        System.out.println(isAnagram(s, t));
+    }
+}
