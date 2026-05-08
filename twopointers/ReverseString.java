143013da59d190b8e37a833acd80b790103efc4a
Adeeb-Sadiq <adeeb5.sadiq7@gmail.com>
2026-05-09 02:11:08 +0530

    reverse the string

---
+++ twopointers/ReverseString.java
@@ -0,0 +1,19 @@
+package twopointers;
+
+public class ReverseString {
+    static void main() {
+        char[] s = {'H', 'E', 'L', 'L', 'O'};
+        reverseString(s);
+        System.out.println(s);
+    }
+
+    public static void reverseString(char[] s) {
+        int i = 0, j = s.length - 1;
+        while(i < j) {
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            i++; j--;
+        }
+    }
+}
