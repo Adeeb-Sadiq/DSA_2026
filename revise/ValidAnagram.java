bbbc1e180485c89325f4d3d3964a02a4e2d61d52
Adeeb-Sadiq <adeeb5.sadiq7@gmail.com>
2026-06-11 19:38:41 +0530

    neetcode valid anagram

---
+++ revise/ValidAnagram.java
@@ -0,0 +1,24 @@
+package revise;
+
+import java.util.HashMap;
+
+// neetcode - https://neetcode.io/problems/is-anagram/question
+public class ValidAnagram {
+    static void main() {
+        System.out.println(isAnagram("racecar", "carrace"));
+        System.out.println(isAnagram("jam", "jar"));
+        System.out.println(isAnagram("racecar", "carracing"));
+    }
+
+    public static boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(char c: s.toCharArray()) map.put(c, map.getOrDefault(c, 0) + 1);
+        for(char c: t.toCharArray()) {
+            if(!map.containsKey(c)) return false;
+            map.put(c, map.get(c) - 1);
+            if(map.get(c) == 0) map.remove(c);
+        }
+        return map.isEmpty();
+    }
+}
