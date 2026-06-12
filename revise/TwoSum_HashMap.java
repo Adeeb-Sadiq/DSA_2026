5916c1da07828f88a4f2f1a3fa15eab7a7a4fb3b
Adeeb-Sadiq <adeeb5.sadiq7@gmail.com>
2026-06-12 15:39:02 +0530

    two sums using hashmap

---
+++ revise/TwoSum_HashMap.java
@@ -0,0 +1,27 @@
+package revise;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class TwoSum_HashMap {
+    static void main() {
+        System.out.println(Arrays.toString(twoSum(new int[]{3,4,5,6}, 7)));
+        System.out.println(Arrays.toString(twoSum(new int[]{3,4,5,6}, 11)));
+        System.out.println(Arrays.toString(twoSum(new int[]{3,4,5,6}, 20)));
+        System.out.println(Arrays.toString(twoSum(new int[]{3,4,5,6}, 9)));
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if(map.containsKey(complement)) {
+                return new int[] {map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+
+        return new int[] {-1, -1};
+    }
+}
