0d1090a8d36fe38846f08c0a25b9c5411b51ca2a
Adeeb-Sadiq <adeeb5.sadiq7@gmail.com>
2026-06-13 23:34:16 +0530

    three sum

---
+++ revise/ThreeSum.java
@@ -0,0 +1,37 @@
+package revise;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+// https://neetcode.io/problems/three-integer-sum/question
+public class ThreeSum {
+    static void main() {
+        System.out.println(threeSum(new int[] {-1,0,1,2,-1,-4}));
+    }
+
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        int n = nums.length;
+        for(int i = 0; i < n - 2; i++) {
+            if(i > 0 && nums[i] == nums[i - 1]) continue;
+            int left = i + 1;
+            int right = n - 1;
+            while(left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if(sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++;
+                    right--;
+                    while(left < right && nums[left] == nums[left - 1]) left++;
+                    while(left < right && nums[right] == nums[right - 1]) right--;
+                } else if(sum < 0) left++;
+                else right--;
+            }
+        }
+
+        return result;
+    }
+}
