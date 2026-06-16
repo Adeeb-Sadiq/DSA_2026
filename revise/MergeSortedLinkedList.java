4b95fe91bae5d83afe2ec03c6c77b1a9a6f2fc93
Adeeb-Sadiq <adeeb5.sadiq7@gmail.com>
2026-06-16 20:58:55 +0530

    merging two linkedlist update

--- revise/MergeSortedLinkedList.java
+++ revise/MergeSortedLinkedList.java
@@ -15,12 +15,11 @@ public class MergeSortedLinkedList {
             if(list1.val <= list2.val) {
                 curr.next = list1;
                 list1 = list1.next;
-                curr = curr.next;
             } else {
                 curr.next = list2;
                 list2 = list2.next;
-                curr = curr.next;
             }
+            curr = curr.next;
         }
         while(list1 != null) {
             curr.next = list1;
