c32a31ecf3943bf96a4afaba0c1e0e3ce25a87c6
Adeeb-Sadiq <adeeb5.sadiq7@gmail.com>
2026-06-23 00:51:21 +0530

    subsets numbers

---
+++ subsets/IntegerSubset.java
@@ -0,0 +1,26 @@
+package subsets;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IntegerSubset {
+    static void main() {
+        int[] nums = {1, 2, 3};
+        System.out.println(subSets(nums));
+    }
+
+    public static List<List<Integer>> subSets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        backTrack(nums, 0, new ArrayList<>(), result);
+        return result;
+    }
+
+    private static void backTrack(int[] nums, int start, List<Integer> current, List<List<Integer>> result) {
+        result.add(new ArrayList<>(current));
+        for(int i = start; i < nums.length; i++) {
+            current.add(nums[i]);
+            backTrack(nums, i + 1, current, result);
+            current.remove(current.size() - 1);
+        }
+    }
+}
