e791f83f1688ea858cdccf98b71215643ef0fc99
Adeeb-Sadiq <adeeb5.sadiq7@gmail.com>
2026-06-22 16:56:18 +0530

    subsets return list of lists

--- revise/SubSetsCharacter.java
+++ revise/SubSetsCharacter.java
@@ -1,9 +1,13 @@
 package revise;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class SubSetsCharacter {
     static void main() {
         subsets("", "abc");
+        System.out.println();
+        System.out.println(returnList(new ArrayList<>(), "abc"));
     }
 
     public static void subsets(String p, String up) {
@@ -13,5 +17,19 @@ public class SubSetsCharacter {
         }
         subsets(p + up.charAt(0), up.substring(1));
         subsets(p, up.substring(1));
+    }
+
+    public static List<List<Character>> returnList(List<Character> p, String up) {
+        if(up.isEmpty()) {
+            List<List<Character>> result = new ArrayList<>();
+            result.add(p);
+            return result;
+        }
+        List<Character> include = new ArrayList<>(p);
+        include.add(up.charAt(0));
+        List<List<Character>> left = returnList(include, up.substring(1));
+        List<List<Character>> right = returnList(p, up.substring(1));
+        left.addAll(right);
+        return left;
     }
 }
