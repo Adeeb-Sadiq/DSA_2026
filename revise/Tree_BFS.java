e8d3a679aa03258665378808c3022df954da02df
Adeeb-Sadiq <adeeb5.sadiq7@gmail.com>
2026-07-17 17:44:19 +0530

    grouping by levels (bfs)

--- revise/Tree_BFS.java
+++ revise/Tree_BFS.java
@@ -6,7 +6,10 @@ import java.util.Queue;
 public class Tree_BFS {
     static void main() {
         TreeNode root = TreeNode.getTree();
+        System.out.println("Traverse:");
         bfs(root);
+        System.out.println("\n\nLevels:");
+        levels(root);
     }
 
     static void bfs(TreeNode root) {
@@ -18,6 +21,27 @@ public class Tree_BFS {
             System.out.print(node.val + " ");
             if(node.left != null) q.offer(node.left);
             if(node.right != null) q.offer(node.right);
+        }
+    }
+
+    static void levels(TreeNode root) {
+        if(root == null) return;
+        int level = 0;
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+
+        while(!q.isEmpty()) {
+            int size = q.size();
+            System.out.print(level + ": ");
+            for(int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                System.out.print(node.val + " ");
+                if(node.left != null) q.offer(node.left);
+                if(node.right != null) q.offer(node.right);
+            }
+            level++;
+            System.out.println();
         }
     }
 }
