20d35d3ff5a5dc50bb997616824075c38aee4959
Adeeb-Sadiq <adeeb5.sadiq7@gmail.com>
2026-07-20 01:07:29 +0530

    tabulation

--- dynamic_programming/Fibonacci.java
+++ dynamic_programming/Fibonacci.java
@@ -4,11 +4,12 @@ import java.util.Arrays;
 
 public class Fibonacci {
     static void main() {
-        System.out.println(fib(5));
-        System.out.println(fib(10));
-        System.out.println(fib(44));
+        int n = 45;
+        System.out.println(fib(n));
+        System.out.println(fibonacci_1(n));
     }
 
+    // Memoization
     public static int fib(int n) {
         int[] dp = new int[n + 1];
         Arrays.fill(dp, -1);
@@ -19,6 +20,20 @@ public class Fibonacci {
         if(n <= 1) return n;
         if(dp[n] != -1) return dp[n];
         dp[n] = fib(n - 1, dp) + fib(n - 2, dp);
+        return dp[n];
+    }
+
+    // Tabulation
+    public static int fibonacci_1(int n) {
+        if(n <= 1) return n;
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for(int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
         return dp[n];
     }
 }
