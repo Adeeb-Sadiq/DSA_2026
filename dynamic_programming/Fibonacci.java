ca8c44c5c9415ac7e0275ebe22383e353b108882
Adeeb-Sadiq <adeeb5.sadiq7@gmail.com>
2026-07-20 01:11:05 +0530

    space optimization O(1)

--- dynamic_programming/Fibonacci.java
+++ dynamic_programming/Fibonacci.java
@@ -7,6 +7,7 @@ public class Fibonacci {
         int n = 45;
         System.out.println(fib(n));
         System.out.println(fibonacci_1(n));
+        System.out.println(fibonacci(n));
     }
 
     // Memoization
@@ -35,5 +36,20 @@ public class Fibonacci {
         }
 
         return dp[n];
+    }
+
+    // space optimization
+    public static int fibonacci(int n) {
+        if(n <= 1) return n;
+        int prev2 = 0;
+        int prev1 = 1;
+
+        for(int i = 2; i <= n; i++) {
+            int curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return prev1;
     }
 }
